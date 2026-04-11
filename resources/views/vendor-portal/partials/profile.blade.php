b982b221c7b4b4ed19224de7b3401cd194ed6f6a
iazza <iazza@example.test>
2026-04-11 14:07:33 +0800

    Fix vendor listings shortcuts and split vendor profile sections

--- resources/views/vendor-portal/partials/profile.blade.php
+++ resources/views/vendor-portal/partials/profile.blade.php
@@ -12,20 +12,34 @@
         $selectedCategoryLabels = collect($selectedVendorCategories ?? [])->map(fn ($key) => (string) ($vendorCategoryMap[$key] ?? $key))->values();
         $approvedCategoryLabels = collect($vendorProfile['approved_categories'] ?? [])->map(fn ($key) => (string) ($vendorCategoryMap[$key] ?? $key))->values();
         $billingRow = $vendorBilling ?? null;
+        $profileSectionQuery = strtolower(trim((string) request()->query('section', 'profile')));
+        $allowedProfileSections = ['profile', 'categories', 'banking', 'address', 'password', 'all'];
+        $activeProfileSection = in_array($profileSectionQuery, $allowedProfileSections, true) ? $profileSectionQuery : 'profile';
     @endphp
 
     <p class="label">My Account</p>
     <div class="panel-links" aria-label="My account sections">
-        <a href="#vendorProfileInformation">Profile &amp; Business</a>
-        <a href="#vendorCategoryWizard">Category Requests</a>
-        <a href="#vendorBankCardSection">Bank &amp; Cards</a>
-        <a href="#vendorAddressSection">Address</a>
-        <a href="#vendorPasswordSection">Password</a>
+        <a href="/vendor/profile?section=profile" class="{{ $activeProfileSection === 'profile' ? 'is-active' : '' }}">Profile &amp; Business</a>
+        <a href="/vendor/profile?section=categories" class="{{ $activeProfileSection === 'categories' ? 'is-active' : '' }}">Category Requests</a>
+        <a href="/vendor/profile?section=banking" class="{{ $activeProfileSection === 'banking' ? 'is-active' : '' }}">Bank &amp; Cards</a>
+        <a href="/vendor/profile?section=address" class="{{ $activeProfileSection === 'address' ? 'is-active' : '' }}">Address</a>
+        <a href="/vendor/profile?section=password" class="{{ $activeProfileSection === 'password' ? 'is-active' : '' }}">Password</a>
+        <a href="/vendor/profile?section=all" class="{{ $activeProfileSection === 'all' ? 'is-active' : '' }}">Show All</a>
     </div>
 
-    @include('vendor-portal.partials.profile-sections.profile-business')
-    @include('vendor-portal.partials.profile-sections.category-requests')
-    @include('vendor-portal.partials.profile-sections.bank-cards')
-    @include('vendor-portal.partials.profile-sections.address')
-    @include('vendor-portal.partials.profile-sections.password')
+    @if (in_array($activeProfileSection, ['profile', 'all'], true))
+        @include('vendor-portal.partials.profile-sections.profile-business')
+    @endif
+    @if (in_array($activeProfileSection, ['categories', 'all'], true))
+        @include('vendor-portal.partials.profile-sections.category-requests')
+    @endif
+    @if (in_array($activeProfileSection, ['banking', 'all'], true))
+        @include('vendor-portal.partials.profile-sections.bank-cards')
+    @endif
+    @if (in_array($activeProfileSection, ['address', 'all'], true))
+        @include('vendor-portal.partials.profile-sections.address')
+    @endif
+    @if (in_array($activeProfileSection, ['password', 'all'], true))
+        @include('vendor-portal.partials.profile-sections.password')
+    @endif
 </section>

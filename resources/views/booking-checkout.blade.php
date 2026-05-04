69482a5361dcf575bf14fcd8cb0add5ddfc5b991
iazza <iazza@example.test>
2026-05-04 16:07:15 +0800

    fix: remove mini search from all page headers; location City/Atoll/Country format; gallery white box

--- resources/views/booking-checkout.blade.php
+++ resources/views/booking-checkout.blade.php
@@ -108,7 +108,7 @@
            'injectUniformHeaderScripts' => true,
            'headerNeedsSpacer' => false,
            'headerHideOnScroll' => true,
-           'headerShowSearch' => true,
+           'headerShowSearch' => false,
            'headerSearchAction' => '/catalog/' . $headerCategoryKey,
            'headerSearchValue' => '',
            'headerCategoryLinks' => $headerCategoryLinks,
